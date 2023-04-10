656d4cbd577e7ef8fdbd861f45efaffb96ace4fc
minwachya <minwachya@gmail.com>
2023-04-10 21:08:28 +0900

    second commit

---
+++ src/main/java/com/example/keyringserver/KeyRingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyRingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyRingServerApplication.class, args);
+	}
+
+}
