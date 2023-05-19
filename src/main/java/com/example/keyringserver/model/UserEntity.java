26b871c0dfadbcf1ccab1f6d1076ce20f0202c95
minwachya <minwachya@gmail.com>
2023-05-19 15:45:00 +0900

    [feat] UserEntity 생성

---
+++ src/main/java/com/example/keyringserver/model/UserEntity.java
@@ -0,0 +1,30 @@
+package com.example.keyringserver.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")}) // DDL 생성 시 유니크 제약 조건
+public class UserEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;   // id와 같이 고유한 기능을 함
+
+    @Column(nullable = false)
+    private String password;
+}
